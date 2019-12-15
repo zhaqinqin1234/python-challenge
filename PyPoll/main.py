3ff9bda6c25d377d873cfbdc5670e479e7662a87
qinqin zha <qinqin.zha@gmail.com>
2019-12-15 13:39:52 -0800

    add pyPoll script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,63 +1,56 @@
 # Dependencies
-import os, csv
+import os, csv, numpy as np
 csv_path = os.path.join("..","Resources", "election_data.csv")
 
 # Lists to store data
 voter_ID = []
 county = []
 candidate = []
+candidate_name = []
+candidate_counts = []
 
 
 # Read in the CSV file
-with open(csv_path, newline='', encoding="utf-8") as budget_file:
+with open(csv_path, newline='', encoding="utf-8") as election_file:
     # Split the data on columns
-    csv_reader = csv.reader(budget_file, delimiter =",")
+    csv_reader = csv.reader(election_file, delimiter =",")
     csv_header = next(csv_reader)
     for row in csv_reader:
-        # Add date
-        month.append(row[0])
-        # Add budget 
-        budget.append(int(row[1]))
-    # count total months 
-    total_month = len(month)
-    print(f"Total Month included in the dataset is: {total_month}")
-
-    # Calculate the total budgets
-    for budgets in budget:
-        total_budget +=budgets
-    print(f"The Total amount is: ${total_budget}") 
-
-    # Calculte the budget change
-    for i in range(1,len(budget)):
-        budget_change.append(budget[i] - budget[i-1])
-    #print(len(budget_change))
-
-    # Calculate the average budget change
-    average_change = sum(budget_change)/(len(budget_change)-1)
-    average_change = round(average_change,2)
-
-    print(f"Average change is:${average_change}")
-
-    # Calculate the greatest change
-    greatest_increase = max(budget_change)
-    greatest_decrease = min(budget_change)
-
-    for i in range(0, len(budget_change)):
-        if budget_change[i] == greatest_increase:
-            greateast_increase_month = month[i]
-        elif budget_change[i] == greatest_decrease:
-            greatest_decrease_month = month[i]
+        # Add voter_ID
+        voter_ID.append(row[0])
+        # Add county 
+        county.append(row[1])
+        # Add candidate
+        candidate.append(row[2])
+    # count total votes 
+    total_votes = len(candidate)
+    print(f"Total number of votes cast is: {total_votes}")
 
 
-    print(f"The greatest increase is: ${greatest_increase}")
-    print(f"The greatest decrease is: ${greatest_decrease}")
-    print(f"The greatest increase month is: {greateast_increase_month}")
-    print(f"The greatest decrease month is: {greatest_decrease_month}")
-#Create a dictionary file to write to
+    # find candiates list
+    unique_candidate = set(candidate)
+    candidate_list = list(unique_candidate)
+    print(f"A complete list of candidates are: {candidate_list}")
 
-dict = {"Total months": total_month, "Total amount": total_budget , "Average change": average_change, "The greatest increase": greatest_increase,
-                     "The greatest decrease": greatest_decrease, "The greatest increase month": greateast_increase_month, "The greatest decrease month": greatest_decrease_month}
-f = open('outfile.txt', 'w')
+    # find winner
+    dic = {}
+    for cand in candidate:
+        if cand not in dic:
+            dic[cand] =1
+        else: 
+            dic[cand] = dic[cand] + 1
+    print(dic)
+    max_count = max(zip(dic.values(), dic.keys()))
+    print(f"winner candidate is {max_count}")
+
+    dic["total Votes "] = total_votes
+    dic["winner"] = max_count
+    
+
+# write to text file   
+f = open('outfile.txt', 'w')                    
 writer = csv.writer(f, delimiter = '\t')
-for key, value in dict.items():
+for key, value in dic.items():
     writer.writerow([key] + [value])
+
+    
